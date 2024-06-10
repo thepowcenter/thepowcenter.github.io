1e8a07ce269dee404b909d3d399ce0cd346ccd6f
Richard Lin <richardlin4375@gmail.com>
2024-06-10 16:20:46 -0400

    More Books

--- js/books.js
+++ js/books.js
@@ -1,4 +1,13 @@
 function flowerdisaster() {
     window.open('https://www.amazon.com/Flower-Disaster-Character-Faith-Building-Chapter/dp/B0CCZSXD21', '_blank');
 }
+function flowerdisasterActivities() {
+    window.open('https://www.amazon.com/Flower-Disaster-Bible-Activities-Book/dp/B0CS5PPT9Q/ref=sr_1_1?crid=1FMLQ7EQPF6D7&dib=eyJ2IjoiMSJ9.4AiH0ZoKE757DeZ_7mVuzChVjfPLOAFq8sbvA93rJ_lPKVe-7pfabv7lr4oLZ7NaIKyXDvxmsafbplCXnHy7vou5xAOyrCD2AXnZdu1ilch7aGggUEqsWWDlaix4j-szUvNdAL1CXFa90UdVciWgTFJDQAQbA6XwPMevyLyhLDbW8bAiE6M9vBYe_AQrribYpUIw-7x45IortrZy_Yc1-MFEND0ycwESzIbQNW_iLGE.SkPvU0JgfT0U2BllA3F0XlDf2ofNQMRm9lIOzJMBAzk&dib_tag=se&keywords=flower+disaster+activity+book&qid=1718047586&sprefix=flower+disaster+activity+book%2Caps%2C107&sr=8-1', '_blank');
+}
 
+function mazes() {
+    window.open('https://www.amazon.com/Amazing-Mazes-Kids-Ages-4-6/dp/B0CNGNN633/ref=sr_1_1?crid=23S7PV2S98MG9&dib=eyJ2IjoiMSJ9.FrE5Fj2UeEuaPhjiSXKprMY00LFOAw-b9-iGzJdy4lXaYk9osDJ4NgrDN2B2pjWzTqZmEI2pLLqhV87nyB4yfYNcoQ2_RZFc7hfXhWabvn8GWj_H202xpjFEQoA6c1qfL_hP1ju-ZMbJG0CDPfv_xTXS-Fso0TRoQMZn4dexvvtUjCPs2tF_SrW2ltbSOim4V1UWa5-_ex45OrVdgkO0wKJbdDHq4sjLYCwrqtO2fuA.USLPHTmf5S4FMm_5p-rH_LLUC7batvpKYW9eByjS7pg&dib_tag=se&keywords=amazing+mazes+for+kids+ages+4-6&qid=1718047680&sprefix=amazing+mazes+for+kids+ages+4-6%2Caps%2C121&sr=8-1', '_blank');
+}
+function wordfind() {
+    window.open('https://www.amazon.com/Word-Find-Children-Animals-Searches/dp/B0D12PBFPH/ref=sr_1_1?crid=PUQP6N28IVS2&dib=eyJ2IjoiMSJ9.V2pycxWjJ_wmMZN9deW77V_nO-rVzxFLx0x8oZZ0SL2aAxBGNycVlzs1Gt3pIeTLsq2FR0mOTSN6XyJYFWisWrpYDkxUEydbmRp0feaUgpv-_I3EKJ4uyixThXUpU7DUGFLwb3kDzBusQMmVafdjDZZ0-SfjH0WvchjZSrf2MIsfC6DmFms7cvE5wwzdcI-mAdAxPvqPAj6bWz3qA2m1ZEjsPOvGCPzeF6X_S6G3XVM.4Iws1CbgzFu9K8SidHe6Fs4tvUdVjK-m1WLeCc3hhb8&dib_tag=se&keywords=word+find+farm&qid=1718047739&sprefix=word+find+farm%2Caps%2C118&sr=8-1', '_blank');
+}
